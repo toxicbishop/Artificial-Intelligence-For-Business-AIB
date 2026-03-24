f2a87d803707dcf00eb32b48590411483907ab1b
toxicbishop <pranavarun19@gmail.com>
2026-03-24 09:50:41 +0530

    Refactor presentation generation code

--- 07/07.py
+++ 07/07.py
@@ -13,45 +13,46 @@
 =============================================================================
 """
 
-from pptx import Presentation 
-from pptx.util import Inches 
-from PIL import Image 
-import os 
-#  Create output folder if not exists 
-os.makedirs("output", exist_ok=True) 
-# Create presentation 
-prs = Presentation() 
-# Slide 1 Creation 
-slide_layout = prs.slide_layouts[0] 
-slide = prs.slides.add_slide(slide_layout) 
-slide.shapes.title.text = "AI in Public Relations" 
-slide.placeholders[1].text = "Automating PR and Branding with Python" 
-# Slide 2 Creation 
-slide_layout = prs.slide_layouts[1] 
-slide = prs.slides.add_slide(slide_layout) 
-slide.shapes.title.text = "Applications of AI in PR" 
-tf = slide.placeholders[1].text_frame 
-tf.text = "Media Monitoring" 
-for point in ["Chatbots", "Sentiment Analysis", "Content Generation"]: p = tf.add_paragraph() 
-p.text = point 
-# Slide 3 Creation 
-slide_layout = prs.slide_layouts[5] 
-slide = prs.slides.add_slide(slide_layout) 
-slide.shapes.title.text = "Brand Logo" 
-img_path = "assets/logo.JPG" 
-try: 
-  img = Image.open(img_path) 
-  img.verify() 
-  slide.shapes.add_picture( 
-  img_path, 
-  Inches(2), 
-  Inches(2), 
-  width=Inches(4) 
-) 
-except Exception as e: 
-  print(" Image error:", e) 
-# Save in OUTPUT folder 
-output_file = "output/final_presentation.pptx" 
-prs.save(output_file) 
-print(" PPT saved successfully in OUTPUT folder!") 
-print(" Location:", output_file)
+from pptx import Presentation
+from pptx.util import Inches
+from PIL import Image
+import os
+os.makedirs("output", exist_ok=True)
+prs = Presentation()
+slide_layout = prs.slide_layouts[0]
+slide = prs.slides.add_slide(slide_layout)
+slide.shapes.title.text = "AI in Public Relations"
+slide.placeholders[1].text = "Automating PR and Branding with Python"
+slide_layout = prs.slide_layouts[1]
+slide = prs.slides.add_slide(slide_layout)
+slide.shapes.title.text = "Applications of AI in PR"
+tf = slide.placeholders[1].text_frame
+tf.text = "Media Monitoring"
+for point in ["Chatbots", "Sentiment Analysis", "Content Generation"]:
+    p = tf.add_paragraph()
+    p.text = point
+slide_layout = prs.slide_layouts[5]
+slide = prs.slides.add_slide(slide_layout)
+slide.shapes.title.text = "Brand Logo"
+img_path = "/home/kssem/Program7/assets/logo.png"
+try:
+    img = Image.open(img_path)
+    img.verify()
+
+    slide.shapes.add_picture(
+        img_path,
+        Inches(2),
+        Inches(2),
+        width=Inches(4)
+    )
+except Exception as e:
+    print("Image error:", e)
+output_file = "output/final-presentation.pptx"
+prs.save(output_file)
+print("PPT saved successfully!")
+print("Location:", output_file)
+
+"""output
+PPT saved successfully!
+Location: output/final-presentation.pptx
+"""
